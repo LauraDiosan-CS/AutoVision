797b51e8a602a061b3f00dbfe8356a04c5e27940
Mira-Rotaru <96740056+Mira-Rotaru@users.noreply.github.com>
2023-11-24 20:04:50 +0200

    added training step

---
+++ training/readme.md
@@ -0,0 +1,12 @@
+# Setup for training
+
+## Traffic Signs Detection
+1. move dataset 'trafficSigns' to 'datasets' folder in this project
+2. search '%appdata%' file folder in File Explorer and open
+3. search 'Ultralytics' folder and open
+4. open 'settings.yaml' file and set 'dataset-dir' key with your path for this project as value; <br>
+   your path should look like this: 'C:\Users\...\CarVision\training\datasets'
+5. run 'train.py' & wait 
+
+Results will be saved to 'results\\traffic-signs-detection' folder.
+
---
+++ training/train.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+import torch
+
+
+def train():
+    "Training object detection for trafficSigns dataset using yolov8-nano"
+
+    model = YOLO('models\\yolov8n.pt')  # load a pretrained model (recommended for training)
+    if torch.cuda.is_available():
+        model.cuda()
+        print('running on gpu started...')
+    results = model.train(data='configs\\traffic_signs.yaml', epochs=100, batch=100, imgsz=640, save_dir='results\\traffic-signs-detection')
+
+
+if __name__ == '__main__':
+    train()
