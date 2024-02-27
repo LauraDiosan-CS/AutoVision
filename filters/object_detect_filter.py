8920ab7ef7897890a617013fac34d81faa8cc45a
Mira-Rotaru <96740056+Mira-Rotaru@users.noreply.github.com>
2024-02-27 13:22:04 +0200

    loaded data with signs info

--- filters/object_detect_filter.py
+++ filters/object_detect_filter.py
@@ -25,7 +25,7 @@ class ObjectDetectionFilter(BaseFilter):
             print('running on cpu...')
         
         yolo_results = self.model(data.frame)
-        self.pre_process_result(yolo_results[0], data)
+        data = self.pre_process_result(yolo_results[0], data)
         data.frame = yolo_results[0].plot()
         data.processed_frames.append(data.frame.copy())
         return data
@@ -39,17 +39,24 @@ class SignsDetect(ObjectDetectionFilter):
         data.traffic_signs = []
         labels = result.names
         print('\nresult:', labels)
+
         for object in result:
             prediction_id = int(object.boxes.cls.item())
             prediction_label = labels[prediction_id]
+
             confidence = f'{object.boxes.conf.item():.2f}'
+
             bbox_tensor_cpu = object.boxes.xyxy.cpu()
-            bbox_list = [el for el in bbox_tensor_cpu.tolist()]
-            print('label:', prediction_label)
-            print('conf:', confidence)
-            print('bbox:', object.boxes.xyxy, bbox_list)
+            bbox_list = [f'{el:.4f}' for el in bbox_tensor_cpu.tolist()[0]]
 
+            road_object = RoadObject(bbox= bbox_list, label= prediction_label,
+                                     conf= confidence, distance=0)
+            
+            data.traffic_signs.append(road_object)
 
+        return data
+
+            
 class TrafficLightDetect(ObjectDetectionFilter):
     def __init__(self, video_info: VideoInfo, model_path):
         super().__init__(video_info, model_path)
